7a79af354223bc90d5da1ab3125d4251d528752e
dagothar <dagothar@gmail.com>
2017-05-03 23:05:34 +0200

    ...

--- scripts/app.js
+++ scripts/app.js
@@ -31,14 +31,23 @@ var App = (function() {
   
   App.prototype.run = function() {
     var maze = new Maze(48, 64);
-    maze.make();
+    
     var img = new Image();
     img.src = 'images/dagothar.jpg';
     console.log(img);
     var self = this;
     img.onload = function() { self._backgroundLayer.scene.context.drawImage(img, 0, 0); };
-    self._backgroundLayer.scene.context.scale(0.5, 0.5);
-    maze.render(this._mazeLayer.scene.context, 5, 1, 'rgba(128, 128, 128, 1)', 'rgba(255, 255, 255, 1)');
+    this._backgroundLayer.scene.context.scale(0.5, 0.5);
+    this._backgroundLayer.scene.context.translate(800-240, 600-320);
+    
+    this._mazeLayer.scene.context.translate(400-120, 300-160);
+    
+    setInterval(function() {
+    var maze = new Maze(48, 64);
+    maze.make();
+    self._mazeLayer.scene.context.clearRect(0, 0, 800, 600);
+    maze.render(self._mazeLayer.scene.context, 5, 1, 'rgba(128, 128, 128, 0)', 'rgba(255, 255, 255, 0.2)');
+    }, 100);
   };
   
   
